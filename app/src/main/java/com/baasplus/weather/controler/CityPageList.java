4ec2e17b431c3c62a1a4f55881792778edc9125c
liuchao0206 <liuch0206@gmail.com>
2015-03-14 17:15:49 +0800

    viewpager succ

---
+++ app/src/main/java/com/baasplus/weather/controler/CityPageList.java
@@ -0,0 +1,23 @@
+package com.baasplus.weather.controler;
+
+import com.baasplus.weather.view.CityPage;
+
+import java.util.ArrayList;
+
+/**
+ * Created by abner-l on 15/3/13.
+ */
+public class CityPageList extends ArrayList<CityPage>{
+
+    private static CityPageList cityPageList = null;
+
+    public static CityPageList getInstance() {
+        if (cityPageList == null) {
+            cityPageList = new CityPageList();
+        }
+        return cityPageList;
+    }
+
+    public CityPageList() {
+    }
+}
--- app/src/main/java/com/baasplus/weather/view/MainActivity.java
+++ app/src/main/java/com/baasplus/weather/view/MainActivity.java
@@ -1,22 +1,27 @@
 package com.baasplus.weather.view;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.TextView;
 
 import com.baasplus.weather.R;
+import com.baasplus.weather.adapter.BPFragmentPagerAdapter;
+import com.baasplus.weather.controler.CityPageList;
 import com.baasplus.weather.controler.CitysList;
+import com.baasplus.weather.controler.DetailFragmentList;
 import com.baasplus.weather.define.DefineMessage;
 import com.baasplus.weather.model.City;
 import com.baasplus.weather.model.Weather;
 import com.baasplus.weather.view.SlidingDrawerFragment.NavigationDrawerCallbacks;
 
-public class MainActivity extends FragmentActivity implements NavigationDrawerCallbacks {
+public class MainActivity extends FragmentActivity implements NavigationDrawerCallbacks,DetailFragment.OnFragmentInteractionListener {
 
     public static Handler mHandler;
 
@@ -65,12 +70,62 @@ public class MainActivity extends FragmentActivity implements NavigationDrawerCa
         titileDetailTV = (TextView) findViewById(R.id.title_detail);
         titleMenuTV = (TextView) findViewById(R.id.title_menu);
 
-        tvDetail = (TextView) findViewById(R.id.tv_detail);
 
         mNavigationDrawerFragment = (SlidingDrawerFragment) getSupportFragmentManager().findFragmentById(
                 R.id.navigation_drawer);
 
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+
+        //citys
+        City city1 = new City();
+        city1.city = "suzhou";
+        Weather weather1 = new Weather();
+        weather1.setCityID("1");
+        weather1.setCity("suzhou");
+        city1.weather = weather1;
+
+        City city2 = new City();
+        city2.city = "xuzhou";
+        Weather weather2 = new Weather();
+        weather2.setCityID("1");
+        weather2.setCity("xuzhou");
+        city2.weather = weather2;
+
+//        CityPage cityPage1 = CityPage.newInstace(this,city1);
+//        CityPage cityPage2 = CityPage.newInstace(this,city2);
+
+        CityPage cityPage1 = new CityPage(this);
+        CityPage cityPage2 = new CityPage(this);
+
+        cityPage1.setCity(city1);
+        cityPage1.initView();
+
+        cityPage2.setCity(city2);
+        cityPage2.initView();
+
+        CityPageList cityPages = CityPageList.getInstance();
+        cityPages.add(cityPage1);
+        cityPages.add(cityPage2);
+
+//        PagerAdapter adapter = new PagerAdapter(cityPages);
+
+        DetailFragmentList detailFragments = DetailFragmentList.getInstance();
+
+        DetailFragment detailFragment1 = DetailFragment.newInstance(city1);
+        DetailFragment detailFragment2 = DetailFragment.newInstance(city2);
+
+        detailFragments.add(detailFragment1);
+        detailFragments.add(detailFragment2);
+
+        android.support.v4.app.FragmentManager manager = getSupportFragmentManager();
+//        FragmentManager manager = getFragmentManager();
+        BPFragmentPagerAdapter adapter = new BPFragmentPagerAdapter(manager,detailFragments);
+
+        viewPager.setAdapter(adapter);
+
     }
 
     /**
@@ -168,4 +223,8 @@ public class MainActivity extends FragmentActivity implements NavigationDrawerCa
 
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
