0eaed3ba934f2b24523279b052f01212f2954880
coderpage <820631390@qq.com>
2015-05-03 09:31:48 +0800

    删除冗余代码

--- app/src/main/java/com/coderpage/weather/adapter/BPFragmentPagerAdapter.java
+++ app/src/main/java/com/coderpage/weather/adapter/BPFragmentPagerAdapter.java
@@ -3,6 +3,7 @@ package com.coderpage.weather.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.view.ViewGroup;
 
 import com.coderpage.weather.controler.DetailFragmentList;
